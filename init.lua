4fe0f4c091983d30bee3c01a5fea4a1f426cc98b
mate <matekopaliani12@gmail.com>
2024-03-27 21:43:22 +0400

    addint autopair

--- init.lua
+++ init.lua
@@ -4,6 +4,14 @@ require("autocmd")
 
 require("lazy").setup({
 	"tpope/vim-sleuth",
+
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
 	{
 		"CRAG666/code_runner.nvim",
 		config = function()
