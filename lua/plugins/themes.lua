d8af30f3e5e5bb438cdd6fe22aa0013cda906125
mate <matekopaliani12@gmail.com>
2025-10-24 12:21:57 +0400

    change theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,31 +1,29 @@
 return {
-	"shaunsingh/nord.nvim",
+	"tjdevries/colorbuddy.nvim",
 	config = function()
-		vim.g.nord_contrast = true
-		vim.g.nord_borders = false
-		vim.g.nord_disable_background = false
-		vim.g.nord_italic = true
-		vim.g.nord_uniform_diff_background = true
-		vim.g.nord_bold = false
+		vim.cmd.colorscheme("gruvbuddy")
+		local colorbuddy = require("colorbuddy")
 
-		require("nord").set()
+		colorbuddy.colorscheme("my-colorscheme-name")
 
-		local groups = {
-			"Normal",
-			"NormalNC",
-			"LineNr",
-			"CursorLineNr",
-			"SignColumn",
-			"VertSplit",
-			"StatusLine",
-			"StatusLineNC",
-			"NormalFloat",
-			"Folded",
-			"EndOfBuffer",
-		}
+		local Color = colorbuddy.Color
+		local colors = colorbuddy.colors
+		local Group = colorbuddy.Group
+		local groups = colorbuddy.groups
+		local styles = colorbuddy.styles
 
-		for _, group in ipairs(groups) do
-			vim.cmd(string.format("highlight %s guibg=#000000 ctermbg=0", group))
-		end
+		Color.new("background", "#282c34")
+		Color.new("red", "#cc6666")
+		Color.new("green", "#99cc99")
+		Color.new("yellow", "#f0c674")
+
+		Group.new("Function", colors.yellow, colors.background, styles.bold)
+		Group.new("luaFunctionCall", groups.Function, groups.Function, groups.Function)
+
+		Group.new("Error", colors.red:light(), nil, styles.bold)
+
+		Group.new("italicBoldFunction", colors.green, groups.Function, styles.bold + styles.italic)
+
+		Group.new("boldFunction", colors.yellow, colors.background, groups.italicBoldFunction - styles.italic)
 	end,
 }
